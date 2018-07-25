2e8f933a02fc61bae1551ee0d8242613927e1718
LuYi <luyi985@gmail.com>
2018-07-26 09:25:38 +1000

    graphql non-react works

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/style.scss';
+import ApolloClient from 'apollo-boost';
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+    uri: "http://localhost:3001/graphql"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        hello
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 const App = () => (
     <div>
         <p>React here!</p>
