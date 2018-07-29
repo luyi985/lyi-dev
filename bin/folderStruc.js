8562c635975990c2c2173452317384fc0ecf5191
LuYi <luyi985@gmail.com>
2018-07-29 16:45:18 +1000

    split code in bin cli

---
+++ bin/folderStruc.js
@@ -0,0 +1,31 @@
+const { srcPath, targetPath } = require('./setPath');
+const fs = require('fs-extra');
+const chalk =  require('chalk');
+const folderTemplate = [
+    'client',
+    'server',
+    '__test__',
+    '.babelrc', 
+    '.eslintrc.js',
+    'js.prettierrc',
+    'style.prettierrc',
+    'webpack.config.js',
+    'fileTransformer.js'
+];
+
+const excute = () => {
+    console.log(chalk.blue(`- Copy folders`));
+    folderTemplate.forEach(folder => {
+        try {
+            fs.copySync(`${srcPath}/${folder}`,`${targetPath}/${folder}`)
+            console.log(chalk.green(`-- Success to copy ${folder}`));
+        } catch(e) {
+            console.log(chalk.red(`-- Fail to copy ${folder}`));
+            console.log(e);
+        }
+    })
+}
+
+module.exports = {
+    excute
+}
---
+++ bin/setPath.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const srcPath = path.join(__dirname, '..');
+const targetPath = process.cwd()
+
+module.exports = {
+    srcPath,
+    targetPath
+}
